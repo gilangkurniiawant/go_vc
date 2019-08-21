1b31412e8a372254c8a8f061cc49aaaeeee1485f
Gilang <gkurniiawant@gmail.com>
2019-08-22 02:31:34 +0700

    dsadsa

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(@$_SESSION['bereer']){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content='true' name='MSSmartTagsPreventParsing' />
 
-    <title>Gojek Tool</title>
+    <title>Gojek Tool - By : Gilang K</title>
 	<link rel="shortcut icon" href="assets/log.jpg" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
@@ -43,7 +43,7 @@ if(@$_SESSION['bereer']){
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="">Gojek Tool</a>
+                <a class="navbar-brand" href="">Gojek Tool- By : Gilang K...<</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 
