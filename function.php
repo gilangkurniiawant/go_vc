b40a13f1219335af1ccecb94ab9f880641f9b228
Gilang <gkurniiawant@gmail.com>
2019-08-22 03:38:50 +0700

    dsadsasss2ssdsasass

--- function.php
+++ function.php
@@ -135,7 +135,7 @@ $data['header']=$header;
 $is = curl($data);
 if(json_decode($is['result'],true)){
     if(array_key_exists('errors',$is['result'])){
-
+        echo $is['errors'][0]["message"]; die();
         if($is['errors'][0]["message"]=="Nomor HP ini tidak valid. Coba lagi dengan nomor yang valid, ya."){
             $data['data'] = '{"name":"' . gen_nama() . '","email":"' . gen_email() . '@gmail.com","phone":"+' . $hp . '","signed_up_country":"ID"}';
             $data['url']=$host.'/v5/customers';
