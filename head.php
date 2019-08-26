522afb2c89e029b15e98eddc1636f3208333cdd5
Gilang <gilangkurniawant@gmail.com>
2019-08-26 11:54:28 +0000

    gogogo

--- head.php
+++ head.php
@@ -10,6 +10,14 @@ if ($_SESSION['admin'] == 1) {
     $_SESSION['admin'] = 0;
 }
 */
+
+function gen_uuid()
+{
+
+    return strtoupper(bin2hex(openssl_random_pseudo_bytes(16)));
+}
+
+
 $login_token = isset($_SESSION['login_token']) ? $_SESSION['login_token'] : '';
 if (@$_SESSION['val_pin']) {
     $pin = $_SESSION['val_pin'] == '' ? '' : $_SESSION['val_pin'];
@@ -17,13 +25,19 @@ if (@$_SESSION['val_pin']) {
     $pin = '';
 }
 
-
+if (!@$_SESSION['uuid']) {
+    $_SESSION['uuid'] = gen_uuid();
+}
+$uuid =  $_SESSION['uuid'];
 
 
 $host = "https://api.gojekapi.com";
 
-$header = 'Host: api.gojekapi.com
+$header = 'X-AppId: com.gojek.app
 User-Agent: okhttp/3.12.1
+X-Platform: Android
+X-Sesion-ID : ' . $uuid . '
+X-PushTokenType: FCM
 Accept: application/json
 X-PhoneModel: Samsung,Samsung Galaxy S10+
 X-DeviceOS: Android,9.0
@@ -31,11 +45,9 @@ X-AppVersion: 3.35.1
 X-AppId: com.gojek.app
 pin: ' . $pin . '
 Authorization: Bearer ' . $bereer . '
-Accept-Language: id-ID
+Accept-Language: en-ID
 Content-Type: application/json; charset=UTF-8
 X-AppVersion: 3.16.1
 Connection: keep-alive    
-X-User-Locale: id_ID
-X-Location: -7.556110,110.831670
-X-Location-Accuracy: 3.0
+X-User-Locale: en_ID
 ' . $xid;
